7c09007eda29b116cd85a6f73740470135384724
albayrakibrahim3457 <77547591+albayrakibrahim3457@users.noreply.github.com>
2023-03-16 16:25:28 +0300

    class,interface

---
+++ OOP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, konutKrediManager, tasitKrediManager, esnafKrediManager };
+            List<ILoggerService> loggers = new List<ILoggerService>() { databaseLoggerService, smsLoggerService, fileLoggerService };
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
